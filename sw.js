bc76bd7c0dbfb4e791654b321bb1f7fae2178c3c
BrocCooley <ryan.s.cooley@gmail.com>
2026-03-30 11:02:32 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,45 @@
+// Service Worker for Landscaping Tracker PWA
+var CACHE_NAME = 'landscaping-v6';
+var urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(n) { return n !== CACHE_NAME; })
+          .map(function(n) { return caches.delete(n); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  // Network-first strategy: try network, fall back to cache
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      // Clone and cache successful responses
+      if (response && response.status === 200) {
+        var responseClone = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, responseClone);
+        });
+      }
+      return response;
+    }).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
